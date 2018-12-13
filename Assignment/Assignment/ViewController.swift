025a8b82d107abb10eb3703c4d0e76c5622cd8ee
Iree García <iree@outlook.com>
2018-12-13 00:33:38 -0800

    Progress with data display.
    All but the images

--- Assignment/Assignment/ViewController.swift
+++ Assignment/Assignment/ViewController.swift
@@ -16,18 +16,77 @@ class ViewController: UIViewController {
       super.viewDidLoad()
       
       if let path = Bundle.main.url(forResource: "data", withExtension: "json"),
-         let string = try? String(contentsOf: path),
-         let data = string.data(using: .utf8)
+         let data = try? Data(contentsOf: path)
       {
-//         print(string.replacingOccurrences(of: "^\\s*", with: "\\\\n", options: .regularExpression))
          do {
-            let decoder = JSONDecoder()
-            let person = try JSONDecoder().decode(Person.self, from: data)
-            print(person)
+            person = try JSONDecoder().decode(Person.self, from: data)
          } catch {
             print(error)
          }
       }
    }
+}
 
+
+// MARK: - Table view
+
+extension ViewController: UITableViewDataSource {
+   
+   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+      return person != nil ? 4: 0
+   }
+   
+   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+      let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ObjectCell
+      
+      func display(_ object: BaseObject) {
+         let summ = object.objectSummary
+         cell.nameLabel.text = summ.name
+         cell.typeLabel.text = "\(summ.type), \(summ.color)"
+         cell.descriptionLabel.text = summ.description
+         
+         cell.objectImageView.isHidden = (object.image == nil)
+         //FIXME: display image
+      }
+      
+      switch indexPath.row {
+      case 0:
+         let car = person!.car
+         display(car)
+         
+         cell.detailsLabel.text = """
+         Doors: \(car.doors)
+         Price: \(car.price)
+         Milage: \(car.milage) miles
+         """
+         
+      case 1:
+         display(person!.computer)
+         
+         cell.detailsLabel.text = """
+         Purchased: \(person!.computer.purchaseDate)
+         """
+         
+      case let i where i > 1:
+         let pet = (i == 2 ? person!.cat: person!.dog)
+         display(pet)
+         
+         cell.detailsLabel.text = """
+         Age: \(pet.age) years
+         Favorite toy: \(pet.favoriteToy)
+         """
+         
+      default: break
+      }
+      
+      return cell
+   }
+}
+
+class ObjectCell: UITableViewCell {
+   @IBOutlet weak var nameLabel: UILabel!
+   @IBOutlet weak var typeLabel: UILabel!
+   @IBOutlet weak var descriptionLabel: UILabel!
+   @IBOutlet weak var detailsLabel: UILabel!
+   @IBOutlet weak var objectImageView: UIImageView!
 }
